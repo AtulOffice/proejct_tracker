fdd7b8ed9359cf9a45ea062d68a96304b448f029
AtulOffice <atulpatelgsr@gmail.com>
2025-12-02 21:01:45 +0530

    d

---
+++ admin_side/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+});
+
+// Default route
+app.get("/", (req, res) => res.json({ message: "Welcome to our API" }));
+app.use((req, res) =>
+  res.status(404).json({ success: false, message: "Not Found" })
+);
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled promise Rejection`);
+  server.close(() => {
+    server.exit(1);
+  });
+});
