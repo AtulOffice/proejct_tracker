2ddfbbd8ae2a3765bf64835c491f21901b2cb380
AtulOffice <atulpatelgsr@gmail.com>
2025-12-20 14:11:33 +0530

    d

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -182,26 +182,26 @@ export const updateOrder = async (req, res) => {
     }
 
     const { bookingDate, orderDate, deleveryDate } = data;
-    if (
-      bookingDate &&
-      orderDate &&
-      new Date(orderDate) < new Date(bookingDate)
-    ) {
-      return res.status(400).json({
-        success: false,
-        message: "Order Date cannot be before Booking Date",
-      });
-    }
-    if (
-      orderDate &&
-      deleveryDate &&
-      new Date(deleveryDate) < new Date(orderDate)
-    ) {
-      return res.status(400).json({
-        success: false,
-        message: "Delivery Date cannot be before Order Date",
-      });
-    }
+    // if (
+    //   bookingDate &&
+    //   orderDate &&
+    //   new Date(orderDate) < new Date(bookingDate)
+    // ) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "Order Date cannot be before Booking Date",
+    //   });
+    // }
+    // if (
+    //   orderDate &&
+    //   deleveryDate &&
+    //   new Date(deleveryDate) < new Date(orderDate)
+    // ) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "Delivery Date cannot be before Order Date",
+    //   });
+    // }
 
     const order = await Order.findByIdAndUpdate(_id, data, { new: true });
     if (!order) {
