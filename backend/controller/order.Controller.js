de4f770b2b44cb0f68d896101579261973f0f686
AtulOffice <atulpatelgsr@gmail.com>
2026-02-04 15:50:59 +0530

    d

--- backend/controller/order.Controller.js
+++ backend/controller/order.Controller.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Order from "../models/orderSheet.model.js";
 import ProjectModel from "../models/Project.model.js";
+import { docsVal } from "../utils/docsDummy.js";
 
 export const createOrder = async (req, res) => {
   try {
@@ -63,130 +64,7 @@ export const createOrder = async (req, res) => {
     const order = await Order.create(data);
 
 
-    const docsVal = {
-      CustomerDevDocuments: {
-        pAndIDs: { value: "", date: null },
-        controlPhilosophy: { value: "", date: null },
-        ioList: { value: "", date: null },
-        samaDiagram: { value: "", date: null },
-        instrumentList: { value: "", date: null },
-        cableSchedule: { value: "", date: null },
-        existingDrawing: { value: "", date: null },
-        existingBackup: { value: "", date: null },
 
-        otherDocument: {
-          name: "",
-          value: "",
-          date: null,
-        },
-      },
-
-      SIEVPLDevDocuments: {
-        indent: { value: "", date: null, version: "" },
-        systemConfiguration: { value: "", date: null, version: "" },
-        instrumentdataSheet: { value: "", date: null, version: "" },
-        ApprovedDrowing: { value: "", date: null, version: "" },
-        ioList: { value: "", date: null, version: "" },
-        cableSchedule: { value: "", date: null, version: "" },
-
-        otherDocument: {
-          name: "",
-          value: "",
-          date: null,
-          version: "",
-        },
-      },
-
-      swDevDocumentsforFat: {
-        logicBackup: { value: "", date: null, version: "" },
-        scadaBackup: { value: "", date: null, version: "" },
-
-        otherDocument: {
-          name: "",
-          value: "",
-          date: null,
-          version: "",
-        },
-      },
-
-      inspectionDocuments: {
-        itr: { value: "", date: null, submittedBy: null },
-        fat: { value: "", date: null, submittedBy: null },
-
-        otherDocument: {
-          name: "",
-          value: "",
-          date: null,
-          submittedBy: null,
-        },
-      },
-
-      dispatchDocuments: [
-        {
-          phaseIndex: 1,
-          packingList: { value: "", date: null },
-          invoice: { value: "", date: null },
-          DeleveryChallan: { value: "", date: null },
-
-          otherDocument: {
-            name: "",
-            value: "",
-            date: null,
-            version: "",
-          },
-        },
-      ],
-
-      PostCommisionDocuments: {
-        finalMom: {
-          value: "",
-          date: null,
-          inputVal: "",
-          submittedBy: null,
-          remarks: "",
-        },
-        asBuiltDrawings: {
-          value: "",
-          date: null,
-          inputVal: "",
-          submittedBy: null,
-          remarks: "",
-        },
-        logicBackup: {
-          value: "",
-          date: null,
-          inputVal: "",
-          submittedBy: null,
-          remarks: "",
-        },
-        scadaBackup: {
-          value: "",
-          date: null,
-          inputVal: "",
-          submittedBy: null,
-          remarks: "",
-        },
-        expenseReport: {
-          value: "",
-          date: null,
-          inputVal: "",
-          submittedBy: null,
-          remarks: "",
-        },
-
-        otherDocument: {
-          name: "",
-          value: "",
-          date: null,
-          inputVal: "",
-          submittedBy: null,
-          remarks: "",
-        },
-      },
-      SIEVPLDevDocumentsRemarks: "",
-      CustomerDevDocumentsRemarks: "",
-      lotval: 1
-    };
 
     const project = await ProjectModel.create({
       OrderMongoId: order._id,
---
+++ backend/utils/docsDummy.js
@@ -0,0 +1,124 @@
+export const docsVal = {
+    CustomerDevDocuments: {
+        pAndIDs: { value: "", date: null },
+        controlPhilosophy: { value: "", date: null },
+        ioList: { value: "", date: null },
+        samaDiagram: { value: "", date: null },
+        instrumentList: { value: "", date: null },
+        cableSchedule: { value: "", date: null },
+        existingDrawing: { value: "", date: null },
+        existingBackup: { value: "", date: null },
+
+        otherDocument: {
+            name: "",
+            value: "",
+            date: null,
+        },
+    },
+
+    SIEVPLDevDocuments: {
+        indent: { value: "", date: null, version: "" },
+        systemConfiguration: { value: "", date: null, version: "" },
+        instrumentdataSheet: { value: "", date: null, version: "" },
+        ApprovedDrowing: { value: "", date: null, version: "" },
+        ioList: { value: "", date: null, version: "" },
+        cableSchedule: { value: "", date: null, version: "" },
+
+        otherDocument: {
+            name: "",
+            value: "",
+            date: null,
+            version: "",
+        },
+    },
+
+    swDevDocumentsforFat: {
+        logicBackup: { value: "", date: null, version: "" },
+        scadaBackup: { value: "", date: null, version: "" },
+
+        otherDocument: {
+            name: "",
+            value: "",
+            date: null,
+            version: "",
+        },
+    },
+
+    inspectionDocuments: {
+        itr: { value: "", date: null, submittedBy: null },
+        fat: { value: "", date: null, submittedBy: null },
+
+        otherDocument: {
+            name: "",
+            value: "",
+            date: null,
+            submittedBy: null,
+        },
+    },
+
+    dispatchDocuments: [
+        {
+            phaseIndex: 1,
+            packingList: { value: "", date: null },
+            invoice: { value: "", date: null },
+            DeleveryChallan: { value: "", date: null },
+
+            otherDocument: {
+                name: "",
+                value: "",
+                date: null,
+                version: "",
+            },
+        },
+    ],
+
+    PostCommisionDocuments: {
+        finalMom: {
+            value: "",
+            date: null,
+            inputVal: "",
+            submittedBy: null,
+            remarks: "",
+        },
+        asBuiltDrawings: {
+            value: "",
+            date: null,
+            inputVal: "",
+            submittedBy: null,
+            remarks: "",
+        },
+        logicBackup: {
+            value: "",
+            date: null,
+            inputVal: "",
+            submittedBy: null,
+            remarks: "",
+        },
+        scadaBackup: {
+            value: "",
+            date: null,
+            inputVal: "",
+            submittedBy: null,
+            remarks: "",
+        },
+        expenseReport: {
+            value: "",
+            date: null,
+            inputVal: "",
+            submittedBy: null,
+            remarks: "",
+        },
+
+        otherDocument: {
+            name: "",
+            value: "",
+            date: null,
+            inputVal: "",
+            submittedBy: null,
+            remarks: "",
+        },
+    },
+    SIEVPLDevDocumentsRemarks: "",
+    CustomerDevDocumentsRemarks: "",
+    lotval: 1
+};
